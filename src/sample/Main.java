561bf68e8dd645a320bf389a9684943690d51b3c
DusekM98 <78905048+DusekM98@users.noreply.github.com>
2021-03-12 23:55:18 +0100

    First commit

---
+++ src/sample/Main.java
@@ -0,0 +1,32 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import sample.database.Connector;
+import sample.database.DBSOperations;
+
+import java.sql.Connection;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Connection connection = Connector.connect();
+        //DBSOperations.printAll(connection);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        primaryStage.setTitle("Registrace objednávky");
+        primaryStage.setScene(new Scene(root, 600, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
